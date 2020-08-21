21249198201980f0c8e8121026effef1ffa82479
iamchiranjitdey <chiranjitdey2000@gmail.com>
2020-08-21 13:11:03 +0530

    formatted number

--- index.js
+++ index.js
@@ -8,5 +8,17 @@ function getOutput(){
     return document.getElementById("output-value").innerText;
 }
 function printOutput(num){
-    document.getElementById('output-value').innerText =num;
+    if (num==""){
+        document.getElementById('output-value').innerText = num;
+    }else{
+    document.getElementById('output-value').innerText =getFormattedNumber(num);
+    }
+}
+function getFormattedNumber(num){
+    var n = Number(num);
+    var value= n.toLocaleString("en");
+    return value;
+}
+function reverseNumberFormat(){
+    return Number(num.replace(/, /g, ''));
 }
